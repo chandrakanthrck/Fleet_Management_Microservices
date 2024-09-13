6f5d757d11c30192b20e6c2120c0db18524693c9
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-09-13 15:19:23 -0700

    add all modules

---
+++ eureka-server/src/test/java/com/fleetmanagement/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fleetmanagement.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EurekaServerApplication.class)
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
